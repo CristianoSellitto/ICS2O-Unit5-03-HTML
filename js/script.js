f44988e396a825479a2a375f48091ad24bd7c9a2
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-28 17:01:52 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-03-HTML
+// April 26 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-03-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Finds if the user can watch a R, PG-13, or G rated movie alone with the age group the user selected.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const seventeenorover = document.getElementById("seventeenorover").checked
+  const thirteentosixteen = document.getElementById("thirteentosixteen").checked
+  const twelveorunder = document.getElementById("twelveorunder").checked
+
+  if (seventeenorover == true) {
+    document.getElementById("result").innerHTML = "<h5>You can watch R, PG-13, or G rated movies alone.</h5>"
+  } else if (thirteentosixteen == true) {
+    document.getElementById("result").innerHTML = "<h5>You can watch PG-13 or G rated movies alone.</h5>"
+  } else if (twelveorunder == true) {
+    document.getElementById("result").innerHTML = "<h5>You can watch a G rated movie alone.</h5>"
+  } else {
+    document.getElementById("result").innerHTML = "<h5>Please enter a proper age group.</h5>"
+  }
 }
